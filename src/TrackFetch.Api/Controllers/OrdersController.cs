0adf313c5af734872e4a611889e0225d2491662e
Mariane Algayer <marianealgayer@gmail.com>
2025-04-29 22:15:29 -0300

    feat: orders controller

---
+++ src/TrackFetch.Api/Controllers/OrdersController.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrackFetch.Api.Controllers;
+
+[ApiVersion("1")]
+[ApiController]
+[Route("v{version:apiversion}/[controller]")]
+public class OrdersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public OrdersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetOrdersAsync(
+        [Required][FromQuery] string orderId,
+        CancellationToken cancellationToken)
+    {
+        var output = await _mediator(orderId, cancellationToken);
+
+        return Ok();
+    }
+}
