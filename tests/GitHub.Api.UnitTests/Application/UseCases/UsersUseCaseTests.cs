ff1dada9dfab22079bd1587aca09c5355f248180
Mariane Algayer <marianealgayer@gmail.com>
2025-05-02 21:07:09 -0300

    tests: controller and usecase unit tests

---
+++ tests/GitHub.Api.UnitTests/Application/UseCases/UsersUseCaseTests.cs
@@ -0,0 +1,51 @@
+﻿using FizzWare.NBuilder;
+using FluentAssertions;
+using GitHub.Api.Application.UseCases.Users;
+using GitHub.Api.Application.UseCases.Users.Interfaces;
+using GitHub.Api.Infrastructure.Repositories.ExternalServices.GitHub.Interfaces;
+using GitHub.Api.Infrastructure.Repositories.ExternalServices.GitHub.Responses;
+using GitHub.Api.Infrastructure.Repositories.Options;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using Xunit;
+
+namespace GitHub.Api.UnitTests.Application.UseCases
+{
+    public class UsersUseCaseTests
+	{
+		private readonly IGitHubRepository _gitHubRepository;
+		private readonly IOptions<GitHubOption> _option;
+        private readonly IUsersUseCase _usersUseCase;
+
+        public UsersUseCaseTests()
+        {
+            _gitHubRepository = Substitute.For<IGitHubRepository>();
+            _option = Substitute.For<IOptions<GitHubOption>>();
+            _option.Value.Returns(new GitHubOption()
+            {
+                BaseUrl = "url",
+                GetUsersPath = "/path?since={0}"
+            });
+            _usersUseCase = new UsersUseCase(_gitHubRepository, _option);
+        }
+
+        [Theory(DisplayName = "UseCase - should return a list of users")]
+        [InlineData(0, 46)]
+        public async Task ShouldListGitHubUsers(int since, int lastId)
+        {
+            var input = new UsersInput() { Since = since };
+            var output = Builder<GetUsersResponse>
+                .CreateListOfSize(5)
+                .TheLast(1)
+                .With(u => u.Id = lastId)
+                .Build();
+
+            _gitHubRepository.GetUsersAsync(Arg.Any<int>(), Arg.Any<CancellationToken>()).Returns(output);
+
+            var result = await _usersUseCase.Handle(input, CancellationToken.None);
+
+            result.Users.Should().NotBeNullOrEmpty();
+            result.Next.Should().Contain(lastId.ToString());
+        }
+    }
+}
--- tests/GitHub.Api.UnitTests/Controllers/UsersControllerTests.cs
+++ tests/GitHub.Api.UnitTests/Controllers/UsersControllerTests.cs
@@ -1,9 +1,42 @@
-﻿using System;
+﻿using FizzWare.NBuilder;
+using FluentAssertions;
+using GitHub.Api.Application.UseCases.Users;
+using GitHub.Api.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Xunit;
 
 namespace GitHub.Api.UnitTests.Controllers
 {
     public class UserControllerTests
     {
+        private readonly IMediator _mediator;
+        private readonly UsersController _userController;
+
+        public UserControllerTests()
+        {
+            _mediator = Substitute.For<IMediator>();
+            _userController = new UsersController(_mediator);
+        }
+
+        [Theory(DisplayName = "Controller - users endpoint should return OK")]
+        [InlineData(46)]
+        public async Task ShouldListGitHubUsers(int since)
+        {
+            var input = new UsersInput() { Since = since };
+            var output = Builder<UsersOutput>.CreateNew().Build();
+
+            _mediator.Send(input, Arg.Any<CancellationToken>()).Returns(output);
+
+            var result = await _userController.GetUsersAsync(since, CancellationToken.None);
+
+            result.Should().NotBeNull().And.BeOfType<OkObjectResult>();
+
+            var okResult = result as OkObjectResult;
+
+            okResult.StatusCode.Should().Be(200);
+        }
     }
 }
 
